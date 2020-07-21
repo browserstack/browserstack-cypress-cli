58bca26a53304ab1090c23de292f8c06e5aed26b
nagpalkaran95 <nagpalkaran95@gmail.com>
2020-07-21 13:18:09 +0530

    Make config management developer friendly

--- bin/helpers/config.js
+++ bin/helpers/config.js
@@ -1,18 +1,16 @@
 var config = {};
-config.env = "prod";
-var hosts = {
-    prod: {
-        uploadUrl: `https://api-cloud.browserstack.com/automate-frameworks/cypress/upload`,
-        rails_host: `https://api.browserstack.com`
-    }
-};
-config.uploadUrl = hosts[config.env].uploadUrl;
-config.rails_host = hosts[config.env].rails_host;
+config.env = process.env.NODE_ENV || "production";
+
+// load config based on env
+require('custom-env').env(config.env)
+
+config.uploadUrl = process.env.UPLOAD_URL;
+config.rails_host = process.env.RAILS_HOST;
 config.cypress_v1 = `${config.rails_host}/automate/cypress/v1`;
 config.buildUrl = `${config.cypress_v1}/builds/`;
 config.buildStopUrl = `${config.cypress_v1}/builds/stop/`;
-config.dashboardUrl = `https://automate.browserstack.com/dashboard/v2/builds/`;
-config.usageReportingUrl = `https://eds.browserstack.com:443/send_event_cy_internal`;
+config.dashboardUrl = process.env.DASHBOARD_URL;
+config.usageReportingUrl = process.env.USAGE_REPORTING_URL;
 config.fileName = "tests.zip";
 
 module.exports = config;
