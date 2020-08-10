2ae1f6f81ba49642f0d1142d69470a5e28d3a7b3
nagpalkaran95 <nagpalkaran95@gmail.com>
2020-08-10 13:39:52 +0530

    fix default config issue

--- bin/helpers/config.js
+++ bin/helpers/config.js
@@ -1,16 +1,25 @@
 var config = {};
 config.env = process.env.NODE_ENV || "production";
 
-// load config based on env
-require('custom-env').env(config.env)
+if(config.env === "production") {
+  config.uploadUrl = 'https://api-cloud.browserstack.com/automate-frameworks/cypress/upload';
+  config.rails_host = 'https://api.browserstack.com';
+  config.dashboardUrl = 'https://automate.browserstack.com/dashboard/v2/builds/';
+  config.usageReportingUrl = 'https://eds.browserstack.com:443/send_event_cy_internal';
+} else {
 
-config.uploadUrl = process.env.UPLOAD_URL;
-config.rails_host = process.env.RAILS_HOST;
+  // load config based on env
+  require('custom-env').env(config.env);
+
+  config.uploadUrl = process.env.UPLOAD_URL;
+  config.rails_host = process.env.RAILS_HOST;
+  config.dashboardUrl = process.env.DASHBOARD_URL;
+  config.usageReportingUrl = process.env.USAGE_REPORTING_URL;  
+}
+
 config.cypress_v1 = `${config.rails_host}/automate/cypress/v1`;
 config.buildUrl = `${config.cypress_v1}/builds/`;
 config.buildStopUrl = `${config.cypress_v1}/builds/stop/`;
-config.dashboardUrl = process.env.DASHBOARD_URL;
-config.usageReportingUrl = process.env.USAGE_REPORTING_URL;
 config.fileName = "tests.zip";
 
 module.exports = config;
