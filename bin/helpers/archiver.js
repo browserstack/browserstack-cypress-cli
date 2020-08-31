34fcc6c03063b4ec460b23644e450221c2a22a15
nagpalkaran95 <nagpalkaran95@gmail.com>
2020-08-31 15:31:20 +0530

    remove . from names

--- bin/helpers/archiver.js
+++ bin/helpers/archiver.js
@@ -36,7 +36,7 @@ const archiveSpecs = (runSettings, filePath) => {
 
     archive.pipe(output);
 
-    let allowedFileTypes = ['js', 'json', 'txt', 'ts', 'feature', 'features', '.pdf', '.jpg', '.jpeg', '.png', '.zip'];
+    let allowedFileTypes = ['js', 'json', 'txt', 'ts', 'feature', 'features', 'pdf', 'jpg', 'jpeg', 'png', 'zip'];
     allowedFileTypes.forEach(fileType => {
       archive.glob(`**/*.${fileType}`, { cwd: cypressFolderPath, matchBase: true, ignore: ['node_modules/**', 'package-lock.json', 'package.json', 'browserstack-package.json'] });
     });
