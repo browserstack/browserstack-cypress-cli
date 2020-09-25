3bb6a6b219329abbcfc2ee68d1720bd2fe3932cb
Surya Tripathi <raj.surya19@gmail.com>
2020-09-25 15:51:24 +0530

    Added sync messages in parts

--- bin/helpers/syncRunner.js
+++ bin/helpers/syncRunner.js
@@ -6,8 +6,42 @@ const config = require("./config"),
   request = require('request');
 
 exports.pollBuildStatus = (bsConfig, buildId) => {
+  logBuildDetails().then((data) => {
+    printSpecsStatus();
+  }).then((data) => {
+    printSpecsRunSummary();
+  }).then((data) => {
+    printFailedSpecsDetails();
+  }).then((data) => {
+    printBuildDashboardLink(buildId);
+  }).then((data) => {
+    // success case!
+    return 0; // exit code 0
+  }).catch((err) => {
+    // failed case!
+    return 1; // exit code 1
+  });
+};
 
-  logger.info(Constants.userMessages.BUILD_REPORT_MESSAGE);
-  logger.info(`${config.dashboardUrl}${buildId}`);
-  return 0;
+let logBuildDetails = () => {
+
+};
+
+let printSpecsStatus = () => {
+
+};
+
+let printSpecsRunSummary = () => {
+
+};
+
+let printFailedSpecsDetails = () => {
+
+};
+
+let printBuildDashboardLink = (buildId) => {
+  new Promise((resolve, reject) => {
+    logger.info(Constants.userMessages.BUILD_REPORT_MESSAGE);
+    logger.info(`${config.dashboardUrl}${buildId}`);
+  });
 };
