6d8c43d3e79d52f3724c32440327c4b6711330e3
nagpalkaran95 <nagpalkaran95@gmail.com>
2020-09-28 16:26:40 +0530

    make changes for --sync

--- bin/helpers/syncRunner.js
+++ bin/helpers/syncRunner.js
@@ -5,43 +5,62 @@ const config = require("./config"),
   utils = require("./utils"),
   request = require('request');
 
-exports.pollBuildStatus = (bsConfig, buildId) => {
-  logBuildDetails().then((data) => {
-    printSpecsStatus();
-  }).then((data) => {
-    printSpecsRunSummary();
-  }).then((data) => {
-    printFailedSpecsDetails();
-  }).then((data) => {
-    printBuildDashboardLink(buildId);
-  }).then((data) => {
-    // success case!
-    return 0; // exit code 0
-  }).catch((err) => {
-    // failed case!
-    return 1; // exit code 1
-  });
+exports.pollBuildStatus = (bsConfig, bsConfigPath, buildDetails) => {
+  return new Promise(function (resolve, reject) {
+    logBuildDetails(bsConfig, bsConfigPath, buildDetails);
+    printSpecsStatus()
+      .then((data) => {
+        printSpecsRunSummary();
+      })
+      .then((data) => {
+        printFailedSpecsDetails();
+      })
+      .then((data) => {
+        printBuildDashboardLink(buildDetails.dashboard_url);
+        // success case!
+        resolve(0); // exit code 0
+      })
+      .catch((err) => {
+        // failed case!
+        reject(err); // exit code 1
+      });
+    });
 };
 
-let logBuildDetails = () => {
+let logBuildDetails = (bsConfig, bsConfigPath, buildDetails) => {
+  let parallels_enabled = false;
+  if (bsConfig.run_settings.parallels) {
+    parallels_enabled = true;
+  }
+  let parallelMessage = `Run in parallel: ${parallels_enabled ? 'enabled' : 'disabled'}`;
+  if (parallels_enabled) parallelMessage = parallelMessage + `(attempting to run on ${buildDetails.machines} machines)`;
 
+  // logger.info(`Configuration file: ${bsConfigPath}`);
+  // logger.info(`Cypress config file: ${bsConfig.run_settings.cypress_config_file}`);
+  // logger.info(`Local connection: ${bsConfig.connection_settings.local ? 'enabled' : 'disabled'}`);
+  logger.info(`Browser Combinations: ${buildDetails.combinations}`);
+  logger.info(parallelMessage);
+  logger.info(`BrowserStack Dashboard: ${buildDetails.dashboard_url}`);
 };
 
 let printSpecsStatus = () => {
-
+  return new Promise(function (resolve, reject) {
+    resolve();
+  });
 };
 
 let printSpecsRunSummary = () => {
-
+  return new Promise(function (resolve, reject) {
+    resolve();
+  });
 };
 
 let printFailedSpecsDetails = () => {
-
+  return new Promise(function (resolve, reject) {
+    resolve();
+  });
 };
 
-let printBuildDashboardLink = (buildId) => {
-  new Promise((resolve, reject) => {
-    logger.info(Constants.userMessages.BUILD_REPORT_MESSAGE);
-    logger.info(`${config.dashboardUrl}${buildId}`);
-  });
+let printBuildDashboardLink = (dashboardUrl) => {
+  logger.info(`${Constants.cliMessages.RUN.BUILD_REPORT_MESSAGE}: ${dashboardUrl}`);
 };
