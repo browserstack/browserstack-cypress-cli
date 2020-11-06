331a9147d87983d234a9c196622a81ef8ca4915a
Surya Tripathi <raj.surya19@gmail.com>
2020-11-06 18:27:30 +0530

    Moving the generation part to reporterHTML file

--- bin/commands/generateReport.js
+++ bin/commands/generateReport.js
@@ -5,7 +5,7 @@ const config = require("../helpers/config"),
   logger = require("../helpers/logger").winstonLogger,
   Constants = require("../helpers/constants"),
   utils = require("../helpers/utils"),
-  reportGenerator = require('../helpers/reporter-html').reporterHTML;
+  reportGenerator = require('../helpers/reporterHTML').reportGenerator;
 
 
 module.exports = function info(args) {
@@ -28,71 +28,7 @@ module.exports = function info(args) {
 
     let buildId = args._[1];
 
-    let options = {
-      url: `${config.buildUrl}${buildId}/custom_report`,
-      auth: {
-        user: bsConfig.auth.username,
-        password: bsConfig.auth.access_key,
-      },
-      rejectUnauthorized: false,
-      headers: {
-        'User-Agent': utils.getUserAgent(),
-      },
-    };
-
-    request.get(options, function (err, resp, body) {
-      let message = null;
-      let messageType = null;
-      let errorCode = null;
-      let build;
-
-      if (err) {
-        message = Constants.userMessages.BUILD_INFO_FAILED;
-        messageType = Constants.messageTypes.ERROR;
-        errorCode = 'api_failed_build_info';
-
-        logger.info(message);
-      } else {
-        try {
-          build = JSON.parse(body);
-        } catch (error) {
-          build = null;
-        }
-      }
-
-      if (resp.statusCode == 299) {
-        messageType = Constants.messageTypes.INFO;
-        errorCode = 'api_deprecated';
-
-        if (build) {
-          message = build.message;
-          logger.info(message);
-        } else {
-          message = Constants.userMessages.API_DEPRECATED;
-          logger.info(message);
-        }
-      } else if (resp.statusCode != 200) {
-        messageType = Constants.messageTypes.ERROR;
-        errorCode = 'api_failed_build_generate_report';
-
-        if (build) {
-          message = `${
-            Constants.userMessages.BUILD_GENERATE_REPORT_FAILED.replace('<build-id>>', buildId)
-          } with error: \n${JSON.stringify(build, null, 2)}`;
-          logger.error(message);
-          if (build.message === 'Unauthorized') errorCode = 'api_auth_failed';
-        } else {
-          message = Constants.userMessages.BUILD_GENERATE_REPORT_FAILED.replace('<build-id>>', buildId);
-          logger.error(message);
-        }
-      } else {
-        messageType = Constants.messageTypes.SUCCESS;
-        message = `Report for build: ${buildId} was successfully created.`;
-        reportGenerator(build);
-        logger.info(message);
-      }
-      utils.sendUsageReport(bsConfig, args, message, messageType, errorCode);
-    });
+    reportGenerator(bsConfig, buildId);
   }).catch(function (err) {
     logger.error(err);
     utils.setUsageReportingFlag(null, args.disableUsageReporting);
