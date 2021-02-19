1d810d6b0bdd3e26c71e24f1b43d3b34632401dd
roshan <roshannikam04@gmail.com>
2021-02-19 12:10:53 +0530

    changed the width of spec name column

--- bin/helpers/sync/syncSpecsLogs.js
+++ bin/helpers/sync/syncSpecsLogs.js
@@ -35,7 +35,7 @@ let getTableConfig = () => {
     border: getBorderConfig(),
     columns: {
       1: {alignment: 'center', width: 1},
-      2: {alignment: 'left', width: 100}
+      2: {alignment: 'left', width: 80}
     },
     columnDefault: {
       width: 30,
