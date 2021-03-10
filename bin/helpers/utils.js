fb5f69cd52233c369155dc3ecb56bde3f34d5dfe
Karan Shah <karan.s@browserstack.com>
2021-03-10 19:54:23 +0530

    Updating version to 1.8.0

--- bin/helpers/utils.js
+++ bin/helpers/utils.js
@@ -264,7 +264,7 @@ exports.isParallelValid = (value) => {
 }
 
 exports.getUserAgent = () => {
-  return `BStack-Cypress-CLI/1.7.2 (${os.arch()}/${os.platform()}/${os.release()})`;
+  return `BStack-Cypress-CLI/1.8.0 (${os.arch()}/${os.platform()}/${os.release()})`;
 };
 
 exports.isAbsolute = (configPath) => {
