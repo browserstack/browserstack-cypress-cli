b71b533c3286a3ae9b3d4691c89ce15d93052d9e
Sagar Ganiga <sagar.g@browserstack.com>
2021-03-17 12:22:14 +0530

    update warning message

--- bin/helpers/constants.js
+++ bin/helpers/constants.js
@@ -41,7 +41,7 @@ const userMessages = {
   LOCAL_START_FAILED: "Local Testing setup failed.",
   LOCAL_STOP_FAILED: "Local Binary stop failed.",
   INVALID_LOCAL_MODE_WARNING: "Invalid value specified for local_mode. local_mode: (\"always-on\" | \"on-demand\"). For more info, check out https://www.browserstack.com/docs/automate/cypress/cli-reference",
-  SPEC_LIMIT_WARNING: "There is a chance that you might not see all the results on the dashboard because of your spec count."
+  SPEC_LIMIT_WARNING: "You might not see all your results on the dashboard because of high spec count, please consider reducing the number of spec files in this folder."
 };
 
 const validationMessages = {
