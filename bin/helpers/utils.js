d8551bcb7d9553ce58a0ae5ad401da6ff507958d
Karan Nagpal <karannagpal@Karans-MacBook-Pro.local>
2021-12-08 18:01:33 +0530

    Instrument zip sizes

--- bin/helpers/utils.js
+++ bin/helpers/utils.js
@@ -1012,3 +1012,13 @@ async function processExitHandler(exitData){
   await this.stopLocalBinary(exitData.bsConfig, exitData.bsLocalInstance, exitData.args);
   process.exit(0);
 }
+
+exports.fetchZipSize = (fileName) => {
+  try {
+    let stats = fs.statSync(fileName)
+    return stats.size; // in bytes
+  }
+  catch(err) {
+    return "file not present"
+  }
+}
