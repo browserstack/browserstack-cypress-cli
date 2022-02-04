b0e28236811ce1c3bc7fddc10af111d2879ea53d
Sourav <sourav@Apples-MBP-2.domain.name>
2022-02-04 12:47:04 +0530

    added code comments

--- bin/commands/runs.js
+++ bin/commands/runs.js
@@ -328,12 +328,16 @@ module.exports = function run(args, rawArgs) {
       pkg,
       updateCheckInterval: 1000 * 60 * 60 * 24 * 7,
     });
-    
+
+    // Checks for update on first run. 
+    // Set lastUpdateCheck to 0 to spawn the check update process as notifier sets this to Date.now() for preventing 
+    // the check untill one interval period. It runs once.
     if (!notifier.disabled && Date.now() - notifier.config.get('lastUpdateCheck') < 50) {
       notifier.config.set('lastUpdateCheck', 0);
       notifier.check();
     }
 
+    // Set the config update as notifier clears this after reading.
     if (notifier.update && notifier.update.current !== notifier.update.latest) {
       notifier.config.set('update', notifier.update);
       notifier.notify({isGlobal: true});
