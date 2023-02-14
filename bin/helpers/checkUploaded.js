808df66d1fbd9b4a9052b9c21ca5b3242dc79f9c
Archish <archishthakkar@gmail.com>
2023-02-14 11:28:35 +0530

    :bug: fix when no npm_depdencies flag is added

--- bin/helpers/checkUploaded.js
+++ bin/helpers/checkUploaded.js
@@ -87,7 +87,9 @@ const checkUploadedMd5 = (bsConfig, args, instrumentBlocks) => {
       packageUrlPresent: false,
     };
     const runSettings = bsConfig.run_settings;
-    if (typeof runSettings.npm_dependencies === 'object') {
+    if (runSettings.npm_dependencies != undefined && 
+      Object.keys(obj).length === 0 &&
+      runSettings.npm_dependencies == typeof runSettings.npm_dependencies === 'object') {
       if (!("cypress" in runSettings.npm_dependencies)) {
         logger.warn("Missing cypress not found in npm_dependencies");
         if ("cypress_version" in runSettings && !runSettings.cypress_version.toString().match(Constants.LATEST_VERSION_SYNTAX_REGEX)) {
