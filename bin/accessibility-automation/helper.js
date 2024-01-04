a4c86ce0e96d97afca128048c655ae49820a1c57
07souravkunda <souravkunda00@gmail.com>
2024-01-04 12:19:48 +0530

    chore: comment

--- bin/accessibility-automation/helper.js
+++ bin/accessibility-automation/helper.js
@@ -191,6 +191,8 @@ const getAccessibilityCypressCommandEventListener = () => {
 const setAccessibilityEventListeners = () => {
   try {
     const cypressCommandEventListener = getAccessibilityCypressCommandEventListener();
+
+    // Searching form command.js recursively
     glob(process.cwd() + '/**/cypress/support/*.js', {}, (err, files) => {
       if(err) return logger.debug('EXCEPTION IN BUILD START EVENT : Unable to parse cypress support files');
       files.forEach(file => {
