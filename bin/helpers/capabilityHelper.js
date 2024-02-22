d0adffab99884544b6d1dc0018c75dbf2f753089
07souravkunda <souravkunda00@gmail.com>
2024-02-22 20:19:28 +0530

    chore: logs

--- bin/helpers/capabilityHelper.js
+++ bin/helpers/capabilityHelper.js
@@ -165,7 +165,7 @@ const getAccessibilityPlatforms = (bsConfig) => {
   }
   browserList.forEach((browserDetails, idx) => {
     accessibilityPlatforms[idx] = (browserDetails.accessibility === undefined) ? rootLevelAccessibility : browserDetails.accessibility;
-    if (browserDetails.name !== 'chrome') {
+    if (browserDetails.browser !== 'chrome') {
       logger.warn(`Accessibility Automation will run only on Chrome browsers for ${browserDetails.platform}.`);
     } else if (browserDetails.version && (!browserDetails.version.includes('latest') || browserDetails.version < 94)) {
       logger.warn(`Accessibility Automation will run only on Chrome browser version greater than 94 for ${browserDetails.platform}.`);
