7d66cb1d366ff6f4265a0848f8f31cf8993661e5
07souravkunda <souravkunda00@gmail.com>
2024-07-17 13:20:57 +0530

    init

--- bin/testObservability/helper/helper.js
+++ bin/testObservability/helper/helper.js
@@ -495,6 +495,12 @@ exports.mapTestHooks = (test) => {
   exports.mapTestHooks(test.parent);
 }
 
+const sleep = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  })
+}
+
 exports.batchAndPostEvents = async (eventUrl, kind, data) => {
   const config = {
     headers: {
@@ -506,6 +512,7 @@ exports.batchAndPostEvents = async (eventUrl, kind, data) => {
 
   try {
     const eventsUuids = data.map(eventData => `${eventData.event_type}:${eventData.test_run ? eventData.test_run.uuid : (eventData.hook_run ? eventData.hook_run.uuid : null)}`).join(', ');
+    await sleep();
     exports.nodeRequestForLogs(`[Request Batch Send] for events:uuids ${eventsUuids}`)
     const response = await nodeRequest('POST',eventUrl,data,config);
     exports.nodeRequestForLogs(`[Request Batch Repsonse] ${util.format(response.data)} for events:uuids ${eventsUuids}`)
