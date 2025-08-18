43f1d6db53555548f433dc47c1f1c02c127d2f09
Tanmay Lokhande <tanmay.l@browserstack.com>
2025-08-18 08:58:31 +0530

    bu

--- bin/testHub/testhubUtils.js
+++ bin/testHub/testhubUtils.js
@@ -86,11 +86,15 @@ function getAccessibilitySettings(bsConfig) {
     
     // Check if accessibility is enabled in run_settings
     if (bsConfig.run_settings && bsConfig.run_settings.accessibilityOptions) {
-      accessibilitySettings = bsConfig.run_settings.accessibilityOptions;
+      accessibilitySettings = { ...bsConfig.run_settings.accessibilityOptions };
     }
 
     // Add non-BrowserStack infrastructure settings if needed
     accessibilitySettings = addNonBstackInfraA11ySettings(bsConfig, accessibilitySettings);
+
+    // Note: Don't send scanner version in request - server will provide it
+    // Remove any scanner version that might be set to avoid validation errors
+    delete accessibilitySettings.scannerVersion;
 
     return {
       settings: accessibilitySettings
