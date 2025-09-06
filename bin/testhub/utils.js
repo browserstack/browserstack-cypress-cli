c05e11eff26747f6758d20f2675ab3ac2e0b170a
Tanmay Lokhande <tanmay.l@browserstack.com>
2025-09-06 13:58:17 +0530

    framework name

--- bin/testhub/utils.js
+++ bin/testhub/utils.js
@@ -10,7 +10,7 @@ const isUndefined = (value) => value === undefined || value === null;
 
 exports.getFrameworkDetails = (user_config) => {
   return {
-    frameworkName: "Cypress",
+    frameworkName: "Cypres",
     frameworkVersion: testObservabilityHelper.getPackageVersion(
       "cypress",
       user_config
