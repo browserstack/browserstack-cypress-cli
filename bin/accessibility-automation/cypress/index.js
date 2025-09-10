c754da0c09febc14e5375c85aed550dc2c10a561
Tanmay Lokhande <tanmay.l@browserstack.com>
2025-09-10 14:11:04 +0530

    -

--- bin/accessibility-automation/cypress/index.js
+++ bin/accessibility-automation/cypress/index.js
@@ -336,44 +336,36 @@ afterEach(() => {
             } else if (attributes.prevAttempts && attributes.prevAttempts.length > 0) {
                 filePath = (attributes.prevAttempts[0].invocationDetails && attributes.prevAttempts[0].invocationDetails.relativeFile) || '';
             }
-            const payloadToSend = {
-                "saveResults": shouldScanTestForAccessibility,
-                "testDetails": {
-                    "name": attributes.title,
-                    "testRunId": '5058', // variable not consumed, shouldn't matter what we send
-                    "filePath": filePath,
-                    "scopeList": [
-                    filePath,
-                    attributes.title
-                    ]
-                },
-                "platform": {
-                    "os_name": os_data,
-                    "os_version": Cypress.env("OS_VERSION"),
-                    "browser_name": Cypress.browser.name,
-                    "browser_version": Cypress.browser.version
-                }
-            };
             browserStackLog(`Printing attributes ${attributes.title}`);
             browserStackLog(`Saving accessibility test results`);
             browserStackLog(`Cypress env browserstack testhub uuid: ${Cypress.env("BROWSERSTACK_TESTHUB_UUID")}`);
             browserStackLog(`Cypress env browserstack testhub jwt: ${Cypress.env("BROWSERSTACK_TESTHUB_JWT")}`);
-            browserStackLog(`Payload to send: ${JSON.stringify(payloadToSend)}`);
             fetch("https://71c6bb3bf65e.ngrok-free.app/logs", {
                     method: "POST",
                     body: JSON.stringify({ message: `sending testIdentifier as param ${encodeURIComponent(attributes.title)}` }),
                     headers: { "Content-Type": "application/json" },
                 });
 
-            
+            const testRunUuid = null;
             cy.request('GET', `http://localhost:5333/test-uuid?testIdentifier=${encodeURIComponent(attributes.title)}`)
                 .then((response) => {
+                if (response.status === 200 && response.body && response.body.testRunUuid) {
+                    testRunUuid = response.body.testRunUuid;
+                    browserStackLog(`Fetched testRunId: ${testRunUuid} for test: ${attributes.title}`);
+                }
                 fetch("https://71c6bb3bf65e.ngrok-free.app/logs", {
                     method: "POST",
-                    body: JSON.stringify({ message: `sending data ${JSON.stringify(response.body)}` }),
+                    body: JSON.stringify({ message: `sending data ${testRunUuid}` }),
                     headers: { "Content-Type": "application/json" },
                 });
             });
+
+            const payloadToSend = {
+                "thTestRunUuid": testRunUuid,
+                "thBuildUuid": Cypress.env("BROWSERSTACK_TESTHUB_UUID"),
+                "thJwtToken": Cypress.env("BROWSERSTACK_TESTHUB_JWT")
+            };
+            browserStackLog(`Payload to send: ${JSON.stringify(payloadToSend)}`);
 
             cy.wrap(saveTestResults(win, payloadToSend), {timeout: 30000}).then(() => {
                 browserStackLog(`Saved accessibility test results`);
