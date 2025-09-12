f1a7c7f2c9b0e00e8bd022d10f2812f64b3df72e
Tanmay Lokhande <tanmay.l@browserstack.com>
2025-09-12 13:36:06 +0530

    -

--- bin/testObservability/reporter/index.js
+++ bin/testObservability/reporter/index.js
@@ -255,7 +255,7 @@ class MyReporter {
       }
     });
 
-    const port = 49153;
+    const port = 5347;
     this.httpServer.listen(port, '127.0.0.1', async () => {
       console.log(`Test Observability HTTP server listening on port ${port}`);
       process.env.TEST_OBSERVABILITY_HTTP_PORT = port;
