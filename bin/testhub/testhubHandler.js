6c03dde10fcdc2942b14f79bdad236926bb982f3
Aakash Hotchandani <aakash.h@browserstack.com>
2025-10-17 16:24:26 +0530

    log changes

--- bin/testhub/testhubHandler.js
+++ bin/testhub/testhubHandler.js
@@ -39,10 +39,10 @@ class TestHubHandler {
       const data = await this.generateBuildUpstreamData(user_config);
       const config = this.getConfig(obsUserName, obsAccessKey);
       
-      console.log('[A11Y] Making TestHub API request to:', TESTHUB_CONSTANTS.TESTHUB_BUILD_API);
+      logger.debug('[A11Y] Making TestHub API request to:', TESTHUB_CONSTANTS.TESTHUB_BUILD_API);
       const response = await nodeRequest( "POST", TESTHUB_CONSTANTS.TESTHUB_BUILD_API, data, config);
       
-      console.log('[A11Y] TestHub API response received:', JSON.stringify(response.data?.accessibility || 'No accessibility in response', null, 2));
+      logger.debug('[A11Y] TestHub API response received:', JSON.stringify(response.data?.accessibility || 'No accessibility in response', null, 2));
       const launchData = this.extractDataFromResponse(user_config, data, response, config);
     } catch (error) {
         console.log(error);
@@ -60,7 +60,7 @@ class TestHubHandler {
     const accessibilityOptions = testhubUtils.getAccessibilityOptions(user_config);
     
     // Log what accessibility data is being sent to server
-    console.log('[A11Y] Sending accessibility options to TestHub server:', JSON.stringify(accessibilityOptions, null, 2));
+    logger.debug('[A11Y] Sending accessibility options to TestHub server:', JSON.stringify(accessibilityOptions, null, 2));
     
     const data = {
       project_name: projectName,
@@ -117,20 +117,14 @@ class TestHubHandler {
     const userAccessibilityEnabled = testhubUtils.isAccessibilityEnabled(user_config);
     const serverAutoEnabled = testhubUtils.isAccessibilityInResponse(response.data);
     
-    console.log('[A11Y] C# SDK pattern check:', {
-      'isAccessibilityEnabled': userAccessibilityEnabled,
-      'isAccessibilityInResponse': serverAutoEnabled,
-      'final_decision': userAccessibilityEnabled || serverAutoEnabled
-    });
-    
     if (userAccessibilityEnabled || serverAutoEnabled) {
       // Match C# SDK: bsConfig.accessibility = true; accessibilityAutomation.ProcessAccessibilityResponse(buildCreationResponse);
-      console.log('[A11Y] Enabling accessibility - either user enabled or server auto-enabled');
+      logger.debug('[A11Y] Enabling accessibility - either user enabled or server auto-enabled');
       user_config.run_settings.accessibility = true;
       testhubUtils.setAccessibilityVariables(user_config, response.data);
     } else {
       // Accessibility not enabled by user and not auto-enabled by server
-      console.log('[A11Y] Accessibility not enabled - neither user enabled nor server auto-enabled');
+      logger.debug('[A11Y] Accessibility not enabled - neither user enabled nor server auto-enabled');
       process.env.BROWSERSTACK_TEST_ACCESSIBILITY = 'false';
       testhubUtils.checkAndSetAccessibility(user_config, false);
     }    
